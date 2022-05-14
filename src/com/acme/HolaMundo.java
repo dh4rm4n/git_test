4f12b463ebc1d2ddc4b1d22c8be67fae1418a161
dh4rm4n <dh4rm4n@gmail.com>
2022-05-14 17:50:05 -0500

    Proyecto git_test

---
+++ src/com/acme/HolaMundo.java
@@ -0,0 +1,9 @@
+package com.acme;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Escribe tu nombre: ");
+        msg.saludar(nombre);
+    }
+}
