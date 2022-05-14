eadbff6b6708e9039192d3c20aef1aedfb95aa10
dh4rm4n <dh4rm4n@gmail.com>
2022-05-14 18:47:00 -0500

    Se actualiza HolaMundo en dev

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -3,8 +3,8 @@ package com.acme;
 public class HolaMundo {
     public static void main(String[] args) {
         Mensaje msg = new Mensaje();
-        String nombre = msg.solicitaDatos("Escribe tus nombres: ");
-        String apellido = msg.solicitaDatos("Escribe tus apellidos: ");
+        String nombre = msg.solicitaDatos("Digita tus nombres: ");
+        String apellido = msg.solicitaDatos("Digita tus apellidos: ");
         Persona p1 = new Persona(nombre, apellido);
         msg.saludar(p1);
     }
